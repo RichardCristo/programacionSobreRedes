02c36374caf582580da54220143c903d4b4566d8
salvatori <82117765+paletin8@users.noreply.github.com>
2023-05-11 17:08:37 -0300

    Delete authRuta.ts

--- src/rutas/authRuta.ts
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express';
-const router : Router = Router();
-import { signin, signup, profile } from '../controladores/authController';
-
-router.post('/signup', signup);
-router.post('/signin', signin);
-router.get('/profile', profile);
-
-export default router;
